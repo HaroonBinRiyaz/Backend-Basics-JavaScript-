2a31bd0da48d426e4b39f6c4c1370eabfeca0888
Haroon Riyaz <mirharoonriyaz@gmail.com>
2026-01-27 15:19:46 +0530

    Implement pagination for users listing

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -20,9 +20,25 @@ const registerUser = async (req, res) => {
 
 //GET / users (read all users)
 const readUsers = async (req, res)=>{
-    const users = await User.find().sort({ createdAt : -1});
-    return res.status(200).json({ok:true, count: users.length, data: users});
-}
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const users = await User.find()
+    .sort({ createdAt : -1})
+    .skip(skip)
+    .limit(limit);
+
+    const total = await User.countDocuments();
+    return res.status(200).json({
+        ok: true,
+        page,
+        limit,
+        total,
+        data: users,
+    });
+    
+};
 
 //GET/users/:id (read one user)
 const getUserById = async (req, res)=>{
