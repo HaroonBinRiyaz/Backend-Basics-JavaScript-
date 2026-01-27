54fd91470f98245c31439e04e9b4d9eae1b55bc3
Haroon Riyaz <mirharoonriyaz@gmail.com>
2026-01-27 13:58:06 +0530

    Corrected mistakes in the initial comments

--- utils/asyncHandler.js
+++ utils/asyncHandler.js
@@ -1,5 +1,5 @@
 /* utils are meant to store helper functions/ repitative functions/code. Here for example,
-writing async contoller function could in update or delete could lead to DB crash, if error happens,
+writing async contoller function in update controller function or delete controller function could lead to DB crash, if error happens,
 hence we normally wrap them in try catch block, however doing that with every controller function is
 repitative, hence a universal function needs to be set that would address all of them */
 
